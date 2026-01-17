a289d7448f17764cce9dbaa82016304ed90824ff
Matthew Kouveliotes <160647852+matthewkouveliotes@users.noreply.github.com>
2026-01-17 14:10:42 -0500

    api

--- server.js
+++ server.js
@@ -71,7 +71,7 @@ app.get('/api/products/', async (req, res) => {
     }
 
     const q = await pool.query(`SELECT * FROM products WHERE upc = '${upc}'`);
-    console.log(q);
+
     const json = JSON.parse(q.rows[0].jsondata);
 
 
@@ -98,7 +98,7 @@ app.post('/api/products/', async (req, res) => {
     const q5 = parseInt(req.body.q5);
 
     const q = await pool.query(`SELECT * FROM products WHERE upc = '${upc}'`)
-
+    console.log(q);
     const json = JSON.parse(q.rows[0].jsondata);
     let product = Object.assign(new ProductClass(), json);
 
